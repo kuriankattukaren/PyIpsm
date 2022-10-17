2f1d84182d251e56951200937dee9faa58852b9a
kuriankattukaren <kuriankattukaren@gmail.com>
2022-10-18 00:58:20 +0800

    Initial upload

--- sample_prosper.py
+++ sample_prosper.py
@@ -1,10 +1,3 @@
-# PyIpsm
-
-PyIpsm is a python package to communicate with Petroleum Experts(Petex) software - Prosper, MBAL and GAP - through openserver using Python.  This Python package eliminates the the need for writing VBA code to interface with Petex software.
-
-## Sample code
-
-```
 import src.ipsm as Ipsm
 
 prosper_exe = "C:\\Program Files\\Petroleum Experts\\IPM 12\\prosper.exe"
@@ -21,5 +14,3 @@ open_file_command = "PROSPER.OPENFILE=C:\\Apps\\src\\Src\\IPSM\\Openserver\\Open
 prosper_error = prosper.commands.doSlow(open_file_command)
 prosper_error = prosper.commands.do("PROSPER.SHUTDOWN")
 print(f"Prosper error: {prosper_error.number}")
-```
-
